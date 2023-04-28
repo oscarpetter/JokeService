0c0d02cd21abc02e4f2f4de65b0ae36bc38335c2
oscarpetter <oscarnordgren94@gmail.com>
2023-04-28 09:13:42 +0200

    implement button function, joke output with addJoke

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-The idea is for this webb application to get data(jokes) from an API.
+The idea is for this web application to get data(jokes) from an API.
 There is an option for different categories in the webb page which will get different jokes from the API.
 Max of ten jokes will be saved on the page, if it reaches ten then the oldest fortune gets removed.
 
---
+++ script.js
@@ -0,0 +1,58 @@
+function onLoad() {
+    addJoke()
+}
+
+window.addEventListener('load', () => {
+    onLoad()
+})
+
+let random = document.getElementById('random')
+random.addEventListener('click', (e) => jokeButtonListner(e))
+
+let short = document.getElementById('categoryTwo')
+short.addEventListener('click', (e) => jokeButtonListner(e))
+
+let shower = document.getElementById('categoryThree')
+shower.addEventListener('click', (e) => jokeButtonListner(e))
+
+let traditional = document.getElementById('categoryFour')
+traditional.addEventListener('click', (e) => jokeButtonListner(e))
+
+
+function jokeButtonListner(e) {
+    console.log(e.currentTarget.id);
+
+    switch (e.currentTarget.id) {
+        case "random":
+            {
+                addJoke("random")
+                break
+            }
+        case "categoryTwo":
+            {
+                addJoke("categoryTwo")
+                break
+            }
+        case "categoryThree":
+            {
+                addJoke("categoryThree")
+                break
+            }
+        case "categoryFour":
+            {
+                addJoke("categoryFour")
+                break
+            }
+    }
+}
+
+function addJoke(type) {
+    const fortunes = document.getElementsByClassName("joke-output")
+
+    for (let i = fortunes.length - 1; i > 0; --i) {
+        fortunes[i].innerText = fortunes[i - 1].innerText
+    }
+
+    /// jokeService async function with get fetch 
+    // fetch api call
+}
